5a4dc8f634d38e9ffdddf5a2d0d976853e746422
m.gonzalez <m.gonzalez@2DAW-04.BUINF.local>
2024-11-28 12:21:52 +0100

    Examen

---
+++ Upstream-mgonzalez/src/main/java/com/gsd/daw/prog/App.java
@@ -0,0 +1,18 @@
+package com.gsd.daw.prog;
+
+import com.gsd.daw.prog.api.UnApi;
+
+/**
+ * Hola Mundo!
+ */
+public class App {
+	/**
+	 * Punto de entrada de la aplicación
+	 *
+	 * @param args
+	 *            no se espera ningún parámetro.
+	 */
+	public static void main(String[] args) {
+		System.out.println(UnApi.holaMundo());
+	}
+}
