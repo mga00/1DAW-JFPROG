d3fe2681c6bbc03e0b48518870810a802246ab0a
m.gonzalez <m.gonzalez@2DAW-04.BUINF.local>
2025-01-13 10:02:12 +0100

    act

--- FigGeoInterfaz/src/main/java/com/gsd/daw/prog/App.java
+++ FigGeoInterfaz/src/main/java/com/gsd/daw/prog/App.java
@@ -4,6 +4,17 @@ public class App {
 	
 	public static void main(String[] args) {
 		
+		FiguraGeometrica [] g1=new FiguraGeometrica[2];
+		g1[0]=new Circulo (2.5);
+		g1[1]=new Rectangulo(4,5);
 		
+		System.out.println("Áreas");
+		for (int i = 0; i < g1.length; i++) {
+			System.out.println(g1[i].calcularArea());
+		}
+		System.out.println("Perímetros");
+		for (int i = 0; i < g1.length; i++) {
+			System.out.println(g1[i].calcularArea());
+		}
 	}
 }
